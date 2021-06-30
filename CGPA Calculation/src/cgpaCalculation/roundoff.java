435ab1fdb71f93ec7c75a979761d1a58ea285815
Mohan <85407008+M0han17@users.noreply.github.com>
2021-06-30 09:53:52 +0530

    modify roundoff.java

--- CGPA Calculation/src/cgpaCalculation/roundoff.java
+++ CGPA Calculation/src/cgpaCalculation/roundoff.java
@@ -1,7 +1,7 @@
 package cgpaCalculation;
 public class roundoff{
    public static float round(float x){
-   if((x < 5.5f)&&(x > 5.0f)){return 5.0f;}
+   if((x < 5.5f)&&(x >= 5.0f)){return 5.0f;}
    else if((x < 6.0f)&&(x >= 5.5f)){return 5.5f;}
    else if((x < 6.5f)&&(x >= 6.0f)){return 6.0f;}
    else if((x < 7.0f)&&(x >= 6.5f)){return 6.5f;}
@@ -11,6 +11,7 @@ public class roundoff{
    else if((x < 9.0f)&&(x >= 8.5f)){return 8.5f;}
    else if((x < 9.5f)&&(x >= 9.0f)){return 9.0f;}
    else if((x < 10.0f)&&(x >= 9.5f)){return 9.5f;}
+   else if(x == 10.0f){return 10.0f;}
    else if((x < 5.0f)||(x >= 10.0f)){System.out.println("Invalid !!");}
    return 0.0f;
 }
