778692bb142d2fb15d8a6d9080b2dd9222039bfa
Mohan <85407008+M0han17@users.noreply.github.com>
2021-07-13 20:19:22 +0530

    Update RoundOff.java

--- CGPA Calculation/src/cgpaCalculation/RoundOff.java
+++ CGPA Calculation/src/cgpaCalculation/RoundOff.java
@@ -1,17 +1,17 @@
 package cgpaCalculation;
 public class RoundOff {
    public static float round(float x){
-   if((x < 5.5f)&&(x >= 5.0f)){return 5.0f;}
-   else if((x < 6.0f)&&(x >= 5.5f)){return 5.5f;}
-   else if((x < 6.5f)&&(x >= 6.0f)){return 6.0f;}
-   else if((x < 7.0f)&&(x >= 6.5f)){return 6.5f;}
-   else if((x < 7.5f)&&(x >= 7.0f)){return 7.0f;}
-   else if((x < 8.0f)&&(x >= 7.5f)){return 7.5f;}
-   else if((x < 8.5f)&&(x >= 8.0f)){return 8.0f;}
-   else if((x < 9.0f)&&(x >= 8.5f)){return 8.5f;}
-   else if((x < 9.5f)&&(x >= 9.0f)){return 9.0f;}
-   else if((x < 10.0f)&&(x >= 9.5f)){return 9.5f;}
-   else if(x == 10.0f){return 10.0f;}
+   if((x < 5.4f)&&(x >= 5.0f)){return 5.0f;}
+   else if((x < 5.85f)&&(x >= 5.4f)){return 5.5f;}
+   else if((x < 6.4f)&&(x >= 5.85f)){return 6.0f;}
+   else if((x < 6.85f)&&(x >= 6.4f)){return 6.5f;}
+   else if((x < 7.4f)&&(x >= 6.85f)){return 7.0f;}
+   else if((x < 7.85f)&&(x >= 7.4f)){return 7.5f;}
+   else if((x < 8.4f)&&(x >= 7.85f)){return 8.0f;}
+   else if((x < 8.85f)&&(x >= 8.4f)){return 8.5f;}
+   else if((x < 9.4f)&&(x >= 8.85f)){return 9.0f;}
+   else if((x < 9.85f)&&(x >= 9.4f)){return 9.5f;}
+   else if(x > 9.85f){return 10.0f;}
    else if((x < 5.0f)||(x >= 10.0f)){System.out.println("Invalid !!");}
    return 0.0f;
 }
